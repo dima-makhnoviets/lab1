3f3c7cc7a26b25a9eedcddb45899cd8e8f8d4e6d
Dimka <dima.makhnoviets@mail.ru>
2019-09-12 10:53:21 +0300

    Give a personalized hello

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -6,8 +6,12 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            Console.WriteLine("Enter your name:");
+            name=Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
             Console.ReadLine();
+
         }
     }
 }
